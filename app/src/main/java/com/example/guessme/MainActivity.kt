eecfbecb8f9702bc8d95d7f8851096659071eede
Lavoyd <aliali20022014@gmail.com>
2022-05-25 12:58:36 +0300

    Initial commit

---
+++ app/src/main/java/com/example/guessme/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.guessme
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+    fun menu(view: View) {
+        val intent = Intent(this, Menu::class.java)
+        startActivity(intent)
+    }
+    fun onClick(view: View) {
+        val intent = Intent(this, Instructions::class.java)
+        startActivity(intent)
+    }
+    fun quit(view: View) {
+        finish()
+    }
+}
